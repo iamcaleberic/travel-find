542494406ec1394502f104eef220377080be7a6b
Eric Caleb <calebme@mail.com>
2016-08-19 06:48:58 +0100

    Added Number helper spec and main example group

---
+++ spec/support/number_helper_spec.rb
@@ -0,0 +1,14 @@
+# encoding: UTF-8
+
+describe "NumberHelper " do
+
+    include NumberHelper
+    describe '#number_to_currency' do
+        context "using default values "do 
+        end
+
+        context "using custom options" do 
+        end
+    end
+
+end
