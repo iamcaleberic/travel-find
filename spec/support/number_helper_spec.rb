0b5ab3e963a437ea4434679aa5a654e7203f932d
Eric Caleb <calebme@mail.com>
2016-08-19 06:58:34 +0100

    Added pending tests to number helper spec

--- spec/support/number_helper_spec.rb
+++ spec/support/number_helper_spec.rb
@@ -5,9 +5,29 @@ describe "NumberHelper " do
     include NumberHelper
     describe '#number_to_currency' do
         context "using default values "do 
+            it "correctly formats an integer" 
+
+            it "correctly formats a float"
+
+            it "correctly formats a string"
+
+            it "uses delimiters for a very large number"
+
+            it "does not have delimiters for small numbers"
         end
 
         context "using custom options" do 
+            it "allows changing the :unit"
+
+            it "allows changing the :precision"
+
+            it "omits the separator if the :precision is 0"
+
+            it "allows changing the :delimiter"
+
+            it "allows changing the separator"
+
+            it "correctly formats using multiple options"
         end
     end
 
