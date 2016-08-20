a8bee5078c23e4e96717070139d1051e50c7f2ce
Eric Caleb <calebme@mail.com>
2016-08-20 06:38:18 +0100

    Added context using custom values to number spec helper

--- spec/support/number_helper_spec.rb
+++ spec/support/number_helper_spec.rb
@@ -28,17 +28,28 @@ describe "NumberHelper " do
         end
 
         context "using custom options" do 
-            it "allows changing the :unit"
+            it "allows changing the :unit" do
+                expect(number_to_currency(2, :unit => '$')).to eq('$2.00')
+            end
+            it "allows changing the :precision" do
+                expect(number_to_currency(4 , :precision => 4)).to  eq(' £ 4.0000')
+            end
 
-            it "allows changing the :precision"
+            it "omits the separator if the :precision is 0" do 
+                expect(number_to_currency(56 , :precision => 0)).to eq(' £ 56')    
+            end
 
-            it "omits the separator if the :precision is 0"
+            it "allows changing the :delimiter" do 
+                expect(number_to_currency(4000 , :delimiter => '/')).to eq(' £ 4/000.00')
+            end
 
-            it "allows changing the :delimiter"
+            it "allows changing the separator" do 
+                expect(number_to_currency(5000 , :separator => '-')).to eq(' £ 5,000-00')
+            end
 
-            it "allows changing the separator"
-
-            it "correctly formats using multiple options"
+            it "correctly formats using multiple options" do
+                expect(number_to_currency(50000 ,:delimiter=> "{" , :precision => 5)).to eq(' £ 50{000.00000')
+            end
         end
     end
 
