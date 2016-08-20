8838b955e8208b231a2205be3941aba8aed80d8c
Eric Caleb <calebme@mail.com>
2016-08-20 06:55:00 +0100

    Added #blank? examples to string extend spec

--- spec/support/string_extend_spec.rb
+++ spec/support/string_extend_spec.rb
@@ -10,5 +10,19 @@ describe 'String' do
         it "capitalizes mixed-case strings"
     end
   
+    describe "#blank?" do
     
+        it "returns true if string is empty"
+
+        it "returns true if String contains only spaces"
+
+        it "returns true of string contains only tabs"
+        # \t\t\t
+
+        it "returns true if string contains only spaces and tabs"
+
+        it "returns false if string contains a letter"
+
+        it "returns false if string contains a number"
+    end 
 end
