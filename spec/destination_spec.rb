1399b0c224c810705e5b6fb18c018619d5b23a66
Eric Caleb <calebme@mail.com>
2016-08-20 08:51:27 +0100

    Added examples for context with default values to destination spec

--- spec/destination_spec.rb
+++ spec/destination_spec.rb
@@ -8,17 +8,36 @@ describe Destination do
         Destination.new(:name => "Hong Kong" , :country => "China" , :price => '1000')
     }
     describe 'attributes' do
-        it "allows reading and writing for :name" 
+        it "allows reading and writing for :name" do
+            expect(hong_kong.name).to eq("Hong Kong")
+        end
 
 
-        it "allows reading and writing for country"
+        it "allows reading and writing for country" do
+            expect(hong_kong.country).to eq("China")
+        end
     end
 
     describe '#initialize ' do 
-        context 'with no options' 
+        
+        context 'with no options' do
+            let(:no_options ){Destination.new}
+
+            it "sets a default of "" for :name " do
+                expect(no_options.name).to eq("")
+            end
+
+            it "sets a default of "" for :country" do
+                expect(no_options.country).to eq("")
+            end
+
+            it " sets a default  of "" for :price" do
+                expect(no_options.price).to eq("")
+            end
         end
 
         context "with custom options" do
+        
         end 
     
     end
