f79485260abfb22b3f5b7154eab268783ce83dd2
Eric Caleb <calebme@mail.com>
2016-08-20 07:23:06 +0100

    Added #titleize examples

--- spec/support/string_extend_spec.rb
+++ spec/support/string_extend_spec.rb
@@ -1,18 +1,25 @@
 describe 'String' do
-
     describe '#titleize' do
-        it "capitalizes each word in a String"
+        it "capitalizes each word in a String" do
+            expect("i am caleb".titleize).to eq("I Am Caleb")
+        end
 
-        it "works with single-word strings"
+        it "works with single-word strings" do
+            expect('killjoys'.titleize).to eq('Killjoys')
+        end
 
-        it "capitalizes all uppercase strings"
+        it "capitalizes all uppercase strings" do
+            expect('DARKMATTER'.titleize).to eq('Darkmatter')
+        end
 
-        it "capitalizes mixed-case strings"
+        it "capitalizes mixed-case strings" do
+            expect("dEAD Of sUMMeR".titleize).to eq('Dead Of Summer')
+        end
     end
   
     describe "#blank?" do
     
-        it "returns true if string is empty"
+        it "returns true if string is empty" 
 
         it "returns true if String contains only spaces"
 
