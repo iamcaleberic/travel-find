f920365ea3c0c60c5638fa021317c755e88068d3
Eric Caleb <calebme@mail.com>
2016-08-20 06:27:02 +0100

    Added context using default values to number spec helper

--- spec/support/number_helper_spec.rb
+++ spec/support/number_helper_spec.rb
@@ -5,15 +5,26 @@ describe "NumberHelper " do
     include NumberHelper
     describe '#number_to_currency' do
         context "using default values "do 
-            it "correctly formats an integer" 
+            it "correctly formats an integer" do
+                expect(number_to_currency(2)).to eq(' £ 2.00')
+            end
+                
 
-            it "correctly formats a float"
+            it "correctly formats a float" do
+                expect(number_to_currency(1.200)).to eq(' £ 1.20')
+            end
 
-            it "correctly formats a string"
+            it "correctly formats a string" do
+                expect(number_to_currency('hello')).to eq(' £ he,llo.00')
+            end
 
-            it "uses delimiters for a very large number"
+            it "uses delimiters for a very large number" do
+                expect(number_to_currency(1000)).to eq(' £ 1,000.00')
+            end
 
-            it "does not have delimiters for small numbers"
+            it "does not have delimiters for small numbers" do
+                expect(number_to_currency(200)).to eq(' £ 200.00')
+            end
         end
 
         context "using custom options" do 
